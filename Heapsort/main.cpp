2927037efa749c52c5f0517e760f8e603d67dbf5
TomDotScott <tom.scott45456@gmail.com>
2020-11-30 12:39:53 +0000

    Rendering the vector
    The vector can now be visualised as bars. These bars are proportional to the width and height of the screen

--- Heapsort/main.cpp
+++ Heapsort/main.cpp
@@ -1,85 +1,120 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <thread>
+
 int random(const int min, const int max)
 {
-    static bool first = true;
-    if (first)
-    {
-        //seeding for the first time only!
-        srand(time(nullptr));
-        first = false;
-    }
-    return min + rand() % ((max) - min);
+	static bool first = true;
+	if (first)
+	{
+		//seeding for the first time only!
+		srand(time(nullptr));
+		first = false;
+	}
+	return min + rand() % ((max)-min);
 }
 
 
 void FisherYatesShuffle(std::vector<int>& vectorToShuffle)
 {
-    for (size_t i = 0; i < vectorToShuffle.size(); ++i)
-    {
-        // Generate a random number and swap the current index
-        // with the random index
-        const auto randomNum = random(0, vectorToShuffle.size());
-        const auto temp = vectorToShuffle[i];
-        vectorToShuffle[i] = vectorToShuffle[randomNum];
-        vectorToShuffle[randomNum] = temp;
-    }
+	for (size_t i = 0; i < vectorToShuffle.size(); ++i)
+	{
+		// Generate a random number and swap the current index
+		// with the random index
+		const auto randomNum{ random(0, vectorToShuffle.size()) };
+		const auto temp{ vectorToShuffle[i] };
+		vectorToShuffle[i] = vectorToShuffle[randomNum];
+		vectorToShuffle[randomNum] = temp;
+	}
 }
 
 void GenerateNumbersVector(std::vector<int>& numbersVector, const int max)
 {
-    for (int i = 0; i < max; ++i)
-    {
-        numbersVector.emplace_back(i);
-    }
+	for (int i = 0; i < max; ++i)
+	{
+		numbersVector.emplace_back(i);
+	}
+}
+
+void Render(const std::vector<int>& collection, const int waitTime, sf::RenderWindow& window)
+{
+	// We must clear the window each time around the loop
+	window.clear();
+
+	const float rectangleWidth{ static_cast<float>(window.getSize().x) / collection.size() - 1 };
+
+	for (unsigned i = 0; i < collection.size(); ++i)
+	{
+		sf::RectangleShape rectangle(
+			{ rectangleWidth, (static_cast<float>(collection[i] + 1) / collection.size()) * (static_cast<float>(window.getSize().y) - 100) }
+		);
+		rectangle.setOrigin(0, rectangle.getGlobalBounds().height);
+
+		rectangle.setFillColor(sf::Color::White);
+
+
+		rectangle.setPosition(static_cast<float>(i) + static_cast<float>(i) * rectangleWidth,static_cast<float>(window.getSize().y));
+		
+		window.draw(rectangle);
+	}
+
+	window.display();
+	std::this_thread::sleep_for(std::chrono::milliseconds(waitTime));
 }
 
 
 int main()
 {
-    std::cout << "How many numbers would you like to deal with?" << std::endl;
-    int n{ 0 };
-    std::cin >> n;
-    std::vector<int> numbers;
-    GenerateNumbersVector(numbers, n);
-    std::cout << "Vector from 1 to n: " << std::endl;
-    for (auto& number : numbers)
-    {
-        std::cout << number << ", ";
-    }
-    std::cout << "\n\n\nShuffled Vector : " << std::endl;
+	sf::RenderWindow window(sf::VideoMode(800, 600), "Heapsort Visualisation");
 
-    FisherYatesShuffle(numbers);
-    for (auto& number : numbers)
-    {
-        std::cout << number << ", ";
-    }
+	std::cout << "How many numbers would you like to deal with?" << std::endl;
+	int n{ 0 };
+	std::cin >> n;
+	std::vector<int> numbers;
+	GenerateNumbersVector(numbers, n);
+	std::cout << "Vector from 1 to n: " << std::endl;
+	for (auto& number : numbers)
+	{
+		std::cout << number << ", ";
+	}
+	Render(numbers, 1000, window);
+	std::cout << "\n\n\nShuffled Vector : " << std::endl;
 
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Heapsort Visualisation");
-    // Main loop that continues until we call window.close()
-    while (window.isOpen())
-    {
-        // Handle any pending SFML events
-        // These cover keyboard, mouse,joystick etc.
-        sf::Event event{};
-        while (window.pollEvent(event))
-        {
-            switch (event.type)
-            {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            default:
-                break;
-            }
-        }
+	FisherYatesShuffle(numbers);
+	for (auto& number : numbers)
+	{
+		std::cout << number << ", ";
+	}
+	Render(numbers, 1000, window);
 
-        // We must clear the window each time around the loop
-        window.clear();
 
-        // Get the window to display its contents
-        window.display();
-    }
+	// Main loop that continues until we call window.close()
+	while (window.isOpen())
+	{
+		// Handle any pending SFML events
+		// These cover keyboard, mouse,joystick etc.
+		sf::Event event{};
+		while (window.pollEvent(event))
+		{
+			switch (event.type)
+			{
+			case sf::Event::Closed:
+				window.close();
+				break;
+			default:
+				break;
+			}
+		}
 
-    return 0;
+		// We must clear the window each time around the loop
+		window.clear();
+
+
+
+
+		// Get the window to display its contents
+		window.display();
+	}
+
+	return 0;
 }
